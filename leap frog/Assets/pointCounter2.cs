94c30a6cb4224d76875b0f547954187ba01526d8
octobervale <valesmith@u.boisestate.edu>
2024-04-19 15:04:51 -0600

    updated game with GUI
    added files for unity

--- leap frog/Assets/pointCounter2.cs
+++ leap frog/Assets/pointCounter2.cs
@@ -1,26 +1,74 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro; // Use TextMeshPro
 
 public class pointCounter2 : MonoBehaviour
 {
-        private int points2 = 0;
-        private bool isCooldown = false;
-        private float cooldownTime = 1.0f;
-        public Text score2Text;
-    
+    private int points2 = 0;
+    private bool isCooldown = false;
+    private float cooldownTime = 1.0f;
+    public TextMeshProUGUI score2Text; // Use TextMeshProUGUI instead of Text
+    public Collider jumpOverTrigger;
+    public Transform player2Transform;
+    public Transform player1Transform; // Reference to the Player1's Transform
+    public TextMeshProUGUI gameOverScreen;
+
+    void Start()
+    {
+        jumpOverTrigger.enabled = false;
+        UpdatePointsText();
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpOverTrigger.enabled = true;
+        }
+        else
+        {
+            jumpOverTrigger.enabled = false;
+        }
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        P1Movement playerMovement = other.gameObject.GetComponent<P1Movement>();
-       // if (playerMovement != null && playerMovement.isJumpOver && !isCooldown)
-       if (other.gameObject.CompareTag("Player2") && !isCooldown)
+
+        if (other.gameObject.CompareTag("Player1"))
         {
-            Debug.Log("triggered 1");
-            points2++;
-            //Debug.Log("Player 2 Points: " + points2);
-            score2Text.text = "Player 2 Points: " + points2;
-            StartCoroutine(Cooldown());
+            // Check if Player2 is higher than Player1
+            if (transform.position.y > player1Transform.position.y && !isCooldown)
+            {
+                Debug.Log("Player2 is higher than Player1");
+
+                points2++;
+                UpdatePointsText();
+                StartCoroutine(Cooldown());
+                //check if game is over
+                CheckGameOver();
+            }
+            else
+            {
+                Debug.Log("Player2 is not higher than Player1");
+            }
+        }
+        else
+        {
+            Debug.Log("Other player does not have Player1 tag");
+        }
+
+    }
+
+    void CheckGameOver()
+    {
+        if (points2 >= 6)
+        {
+            //end the game
+            gameOverScreen.enabled = true;
+            gameOverScreen.text = "game over, player 2 wins!";
+            // Stop the game
+            Time.timeScale = 0;
         }
     }
 
@@ -29,5 +77,10 @@ public class pointCounter2 : MonoBehaviour
         isCooldown = true;
         yield return new WaitForSeconds(cooldownTime);
         isCooldown = false;
+    }
+
+    void UpdatePointsText()
+    {
+        score2Text.text = "Player 2 Points: " + points2.ToString();
     }
 }
